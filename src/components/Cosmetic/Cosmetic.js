48ed444b39ebd32fd6af5d0332bfcaf0ae4bb9e9
saklainMostake <saklain15-12003@diu.edu.bd>
2022-09-27 23:57:39 +0600

    all done

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import './Cosmetic.css'
 
 const Cosmetic = (props) => {
-    const {name, price, id} = props.cosmetic
+    const {name, price, id} = props.cosmetic;
+    const addToCart = (id) =>{
+        console.log(id)
+    }
+    const addToCartParam = () => addToCart(id)
     return (
-        <div>
+        <div className='product'>
             <h2>Buy This: {name} </h2>
             <p>Only For: ${price}</p>
             <p><small>Id: {id} </small></p>
+            <button onClick={addToCartParam}>Add to cart</button>
+            <button onClick={() => addToCart(id)}>Add to cart shortCut</button>
         </div>
     );
 };
--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cosmetic from '../Cosmetic/Cosmetic';
 
 const Cosmetics = () => {
-    const cosmetics = [
-        {id: 1, name: 'Alta', price: '100'},
-        {id: 2, name: 'Palis', price: '200'},
-        {id: 3, name: 'Malis', price: '300'},
-        {id: 4, name: 'Balis', price: '400'},
-        {id: 5, name: 'Nalis', price: '500'}
-    ]
+   const [cosmetics, setCosmetics] = useState([])
+   useEffect( () =>{
+    fetch('data.json')
+    .then(Response => Response.json())
+    .then(  data =>  setCosmetics(data)  )
+   }, [])
     return (
-        <div>
+        <div className='cs'>
             <h1>Welcome to my cosmetics</h1>
             {
                 cosmetics.map(cosmetic => <Cosmetic
